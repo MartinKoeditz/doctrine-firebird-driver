4ec00bdea82495eedcd7d83266d274c8f6132070
Vince <121615999+ITandSynergyDev@users.noreply.github.com>
2022-12-30 14:44:21 +0000

    Added script to launch unit-tests

--- startUnitTests.sh
+++ startUnitTests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./bin/phpunit -c tests/phpunit.xml --bootstrap ./vendor/autoload.php  tests/ 2>&1
+php8.1 ./bin/phpunit -c tests/phpunit.xml --bootstrap ./vendor/autoload.php  tests/ 2>&1
